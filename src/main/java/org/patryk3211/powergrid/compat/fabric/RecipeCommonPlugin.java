43ef4c6eea6f598b5717a983d826fd0b23295617
chaevsfe <pie@duck.com>
2026-09-22 19:27:54 -0400

    Synchronize the magnetizing and boosting recipes to clients with JEI or RRV

---
+++ src/main/java/org/patryk3211/powergrid/compat/fabric/RecipeCommonPlugin.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2026 chaevsfe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.patryk3211.powergrid.compat.fabric;
+
+import net.fabricmc.fabric.api.recipe.v1.sync.RecipeSynchronization;
+import org.patryk3211.powergrid.collections.ModdedRecipeTypes;
+
+public final class RecipeCommonPlugin {
+    private RecipeCommonPlugin() {
+    }
+
+    public static void register() {
+        RecipeSynchronization.synchronizeRecipeSerializer(ModdedRecipeTypes.MAGNETIZATION_SERIALIZER);
+        RecipeSynchronization.synchronizeRecipeSerializer(ModdedRecipeTypes.BOOSTING_SERIALIZER);
+    }
+}

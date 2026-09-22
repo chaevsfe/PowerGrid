b9d5b628a80890bbc43fdaab1d61ad8bb32af633
chaevsfe <pie@duck.com>
2026-09-22 15:17:40 -0400

    Join copper plating stairs and slabs to the plating's connected texture

--- src/main/java/org/patryk3211/powergrid/collections/ModdedModels.java
+++ src/main/java/org/patryk3211/powergrid/collections/ModdedModels.java
@@ -25,7 +25,14 @@ import com.zurrtum.create.client.infrastructure.model.CTModel;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.rendering.v1.BlockColorRegistry;
+import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SlabBlock;
+import net.minecraft.world.level.block.StairBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.Half;
+import net.minecraft.world.level.block.state.properties.SlabType;
+import net.minecraft.world.level.block.state.properties.StairsShape;
 import org.patryk3211.powergrid.circuits.circuitboard.CircuitBoardModel;
 import org.patryk3211.powergrid.electricity.battery.BatteryCTBehaviour;
 import org.patryk3211.powergrid.electricity.solarpanel.SolarPanelCTBehaviour;
@@ -37,6 +44,8 @@ public class ModdedModels {
         connectedTextures(ModdedBlocks.BATTERY.get(), new BatteryCTBehaviour());
         AllCasings.make(ModdedBlocks.CONDUCTIVE_CASING.get(), ModdedPartialModels.CONDUCTIVE_CASING);
         AllCasings.make(ModdedBlocks.COPPER_PLATING.get(), ModdedPartialModels.COPPER_PLATING);
+        AllCasings.make(ModdedBlocks.COPPER_PLATING_STAIRS.get(), ModdedPartialModels.COPPER_PLATING, ModdedModels::stairsFaceConnects);
+        AllCasings.make(ModdedBlocks.COPPER_PLATING_SLAB.get(), ModdedPartialModels.COPPER_PLATING, ModdedModels::slabFaceConnects);
         connectedTextures(ModdedBlocks.CONDUCTIVE_CASING.get(), new EncasedCTBehaviour(ModdedPartialModels.CONDUCTIVE_CASING));
         connectedTextures(ModdedBlocks.COPPER_PLATING.get(), new EncasedCTBehaviour(ModdedPartialModels.COPPER_PLATING));
         connectedTextures(ModdedBlocks.COPPER_PLATING_STAIRS.get(), new EncasedCTBehaviour(ModdedPartialModels.COPPER_PLATING));
@@ -46,6 +55,28 @@ public class ModdedModels {
         AllModels.register(ModdedBlocks.CIRCUIT_BOARD.get(), CircuitBoardModel.of());
         BlockColorRegistry.register(CircuitBoardModel.tintSources(), ModdedBlocks.CIRCUIT_BOARD.get());
         AllFluidConfigs.model(ModdedFluids.ACID);
+    }
+
+    private static boolean stairsFaceConnects(BlockState state, Direction face) {
+        var shape = state.getValue(StairBlock.SHAPE);
+        var half = state.getValue(StairBlock.HALF);
+        var facing = state.getValue(StairBlock.FACING);
+        if(half == Half.TOP && face == Direction.UP)
+            return true;
+        if(half == Half.BOTTOM && face == Direction.DOWN)
+            return true;
+        if(facing == face)
+            return true;
+        if(shape == StairsShape.INNER_LEFT && face == facing.getCounterClockWise())
+            return true;
+        return shape == StairsShape.INNER_RIGHT && face == facing.getClockWise();
+    }
+
+    private static boolean slabFaceConnects(BlockState state, Direction face) {
+        var type = state.getValue(SlabBlock.TYPE);
+        if(type == SlabType.TOP && face == Direction.UP)
+            return true;
+        return type == SlabType.BOTTOM && face == Direction.DOWN;
     }
 
     private static void connectedTextures(Block block, ConnectedTextureBehaviour behaviour) {
